620ecb2f8ade45cc6439ba40ddd403e4d278c5f5
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-12-13 09:40:41 -0300

    initial commit

---
+++ src/main/java/brewery/restdoc/RestDocApplication.java
@@ -0,0 +1,13 @@
+package brewery.restdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brewery/restdoc/RestDocApplicationTests.java
@@ -0,0 +1,13 @@
+package brewery.restdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
